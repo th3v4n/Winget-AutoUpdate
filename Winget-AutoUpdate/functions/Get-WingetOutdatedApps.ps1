22a13450ac13f80f3a5ee5f230610e673a18b527
Romain <96626929+Romanitho@users.noreply.github.com>
2023-07-20 12:58:53 +0200

    Handle chinese chars

--- Winget-AutoUpdate/functions/Get-WingetOutdatedApps.ps1
+++ Winget-AutoUpdate/functions/Get-WingetOutdatedApps.ps1
@@ -28,8 +28,8 @@ function Get-WingetOutdatedApps {
     #Get header line
     $fl = $fl - 1
 
-    #Get header titles
-    $index = $lines[$fl] -split '\s+'
+    #Get header titles [and manage non latin characters]
+    $index = $($lines[$fl] -replace '[\u4e00-\u9fa5]', '**') -split '\s+'
 
     # Line $fl has the header, we can find char where we find ID and Version
     $idStart = $lines[$fl].IndexOf($index[1])
@@ -44,8 +44,8 @@ function Get-WingetOutdatedApps {
             #Get header line
             $fl = $i - 1
 
-            #Get header titles
-            $index = $lines[$fl] -split '\s+'
+            #Get header titles [and manage non latin characters]
+            $index = $($lines[$fl] -replace '[\u4e00-\u9fa5]', '**') -split '\s+'
 
             # Line $fl has the header, we can find char where we find ID and Version
             $idStart = $lines[$fl].IndexOf($index[1])
@@ -55,10 +55,12 @@ function Get-WingetOutdatedApps {
         #(Alphanumeric | Literal . | Alphanumeric) - the only unique thing in common for lines with applications
         if ($line -match "\w\.\w") {
             $software = [Software]::new()
-            $software.Name = $line.Substring(0, $idStart).TrimEnd()
-            $software.Id = $line.Substring($idStart, $versionStart - $idStart).TrimEnd()
-            $software.Version = $line.Substring($versionStart, $availableStart - $versionStart).TrimEnd()
-            $software.AvailableVersion = $line.Substring($availableStart).TrimEnd()
+            #Manage non latin characters
+            $nameDeclination = $([System.Text.Encoding]::UTF8.GetByteCount($($line.Substring(0, $idStart) -replace '[\u4e00-\u9fa5]', '**')) - $line.Substring(0, $idStart).Length)
+            $software.Name = $line.Substring(0, $idStart - $nameDeclination).TrimEnd()
+            $software.Id = $line.Substring($idStart - $nameDeclination, $versionStart - $idStart - $nameDeclination).TrimEnd()
+            $software.Version = $line.Substring($versionStart - $nameDeclination, $availableStart - $versionStart - $nameDeclination).TrimEnd()
+            $software.AvailableVersion = $line.Substring($availableStart - $nameDeclination).TrimEnd()
             #add formated soft to list
             $upgradeList += $software
         }
